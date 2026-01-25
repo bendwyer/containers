3bc713cb1f3dde78b3b8d29521e250fd141b3714
Ben Dwyer <17102207+bendwyer@users.noreply.github.com>
2026-01-25 15:45:34 +0100

    Add the kobo-book-downloader tool (#1)

---
+++ images/kobo-book-downloader/get-version.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# repo_owner="TnS-hun"
+# repo_name="kobo-book-downloader"
+
+# curl -sSL https://api.github.com/repos/${repo_owner}/${repo_name}/releases/latest | jq -r '.tag_name | split("v")[1]'
+
+echo "1.0.0"
